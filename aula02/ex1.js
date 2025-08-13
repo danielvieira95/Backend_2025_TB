5adfc1d43d6c43ce32b26b1caa81039ae935e7fa
danielvieira95 <engdanielvieira95@gmail.com>
2025-08-13 20:35:18 -0300

    aula 02
    Exemplo hello world Java Script e Node

---
+++ aula02/ex1.js
@@ -0,0 +1,7 @@
+ // Primeiro exemplo de codigo em Java Script
+
+ const idade = 29; // declaro uma constante chamada idade
+ const nome = "Daniel"; // declara constante chamada nome
+
+ // Imprime uma mensagem no terminal
+ console.log("Ola Daniel !");
