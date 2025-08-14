1e2dd224f49d0bf31bc2b2003169dc0e3cc23993
danielvieira95 <engdanielvieira95@gmail.com>
2025-08-13 21:41:53 -0300

    aula 02
    tipos de variavel com typeof

--- aula02/ex1.js
+++ aula02/ex1.js
@@ -2,10 +2,11 @@
 
  const idade = 29; // declaro uma constante chamada idade
  const nome = "Daniel"; // declara constante chamada nome
-
+ const inf = nome + " "+  idade; // junta o nome com a idade
  // Imprime uma mensagem no terminal
  console.log("Ola Daniel !");
 
  // Exibindo o valor das constantes no terminal
 
  console.log(`${nome} idade ${idade}`); // exibe o valor no terminal
+ console.log(inf);
---
+++ aula02/ex2.js
@@ -0,0 +1,8 @@
+// Exemplo 2 verificando o tipo de variavel
+
+const valor = 50.5; // constante do tipo double
+const num = 100; // constante
+const a = true ; // constante do tipo booleana
+console.log(valor,`Tipo da constante ${typeof valor}`); // typeof exibe o tipo da constante ou variavel
+console.log(num,`${typeof num}`);
+console.log(a,`${typeof a}`);
