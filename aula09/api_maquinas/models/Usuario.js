51210ba421034006af4496649918302052cf0db6
danielvieira95 <engdanielvieira95@gmail.com>
2025-10-01 20:03:26 -0300

    aula 09
    Criação do objeto Usuario na pasta models

---
+++ aula09/api_maquinas/models/Usuario.js
@@ -0,0 +1,14 @@
+// Cria o modelo para salvar o usuario
+
+// importa o mongoose para definição do Schema e modelo
+const mongoose = require("mongoose");
+
+// Cria a estrutura schema para o documento de usuario
+const UsuarioSchema = new mongoose.UsuarioSchema({
+    nome:{type: String, required: true}, // Campo nome, obrigatorio e do tipo String
+    email:{type: String, required: true} // Campo email, obrigatorio  e do tipo String
+});
+
+// Exporta o modelo Usuario que será utilizado nas rotas CRUD
+
+module.exports = mongoose.model("Usuario",UsuarioSchema);
