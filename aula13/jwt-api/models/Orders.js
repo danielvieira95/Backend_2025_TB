fcb166a526ee11f91cb78d612f792eab992aa206
daniel vieira <engdanielvieira95@gmail.com>
2025-10-29 20:34:57 -0300

    aula13
    create models Order.js
    Cria o modelo para processar o pedido

---
+++ aula13/jwt-api/models/Orders.js
@@ -0,0 +1,20 @@
+// Criar o modelo para processar os pedidos
+
+const { timeStamp } = require("console");
+const mongoose = require("mongoose"); // importa a biblioteca para realizar comunicação com o mongo
+
+const orderSchema = new mongoose.orderSchema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, ref:"User",require:true},
+        items:[
+            {
+                productId:{type: String, required: true},
+                qtd:{type: Number,required:true,min:1}
+            }
+        ],
+        notes:{type:String}
+    },
+    {timestamps:true}
+);
+
+module.exports = mongoose.model("Order",orderSchema);
