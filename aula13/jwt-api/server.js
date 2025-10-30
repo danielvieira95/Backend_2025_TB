b209e1745cd4c204cbf18d973384040cfd4b9130
daniel vieira <engdanielvieira95@gmail.com>
2025-10-29 22:40:05 -0300

    aula 13
    server.js
    chama as rotas da api

---
+++ aula13/jwt-api/server.js
@@ -0,0 +1,30 @@
+// arquivo principal da nossa api
+
+require("dotenv").config(); // biblioteca para pegar as variaveis do arquivo .env
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors =  require("cors");
+const {connectDB} = require("./db");
+
+const app = express(); 
+
+// middlewares globais
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+// rotas
+
+app.get("/",(req,res)=>res.json({status:"ok"}));
+app.use("/auth",require("./routes/auth"));
+app.use("/orders",require("./routes/orders"))
+
+// inicia o servidor
+const PORT = process.env.PORT || 3000;
+connectDB(process.env.MONGO_URI).then(()=>{app.listen(PORT,()=>console.log(`API rodando em http://localhost:${PORT}`));
+}).catch((err)=>{
+    console.error("Erro ao conectar ao MongoDB",err);
+    process.exit(1);
+})
