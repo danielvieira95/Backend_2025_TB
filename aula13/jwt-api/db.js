6e650c995a9def3dc4b76a7598a8276e639f014d
daniel vieira <engdanielvieira95@gmail.com>
2025-10-29 22:26:23 -0300

    aula 13
    arquivo para conexão com o banco de dados

---
+++ aula13/jwt-api/db.js
@@ -0,0 +1,11 @@
+// conexão com o banco de dados
+
+const mongoose = require("mongoose");
+async function connectDB(uri) {
+    mongoose.set("strictQuery",true);
+    await mongoose.connect(uri);
+    console.log("MongoDB conectado !");
+    
+}
+
+module.exports = {connectDB};
