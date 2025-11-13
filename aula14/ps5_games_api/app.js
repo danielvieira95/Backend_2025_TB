92ebb11d634e417a0ef29d0d157864dce1a24cf9
danielvieira95 <engdanielvieira95@gmail.com>
2025-11-12 22:54:42 -0300

    aula  14
    mongo db local

--- aula14/ps5_games_api/app.js
+++ aula14/ps5_games_api/app.js
@@ -7,14 +7,15 @@ const gameRoutes = require('./routes/games');
 const path = require('path')
 
 const app = express();
-mongoose.connect('mongodb+srv://danielvieira2006:Dan100455@cluster0.j1d4z.mongodb.net/ps5_games1',{
+mongoose.connect("mongodb://localhost:27017/ps5games",
+       {
     useNewUrlParser:true,useUnifiedTopology:true
 }).then(()=>console.log('Mongodb conectado')).catch(err=>console.error('Erro ao conectar no mongo',err));
 
 app.use(bodyParse.json());
 app.use('/uploads',express.static(path.join(__dirname,'uploads')));
 app.use('/api/games',gameRoutes);
-const port = 3000;
+const port = 3001;
 app.listen(port,()=>{
     console.log(`Servidor rodando na porta ${port}`)
 });
